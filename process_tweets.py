310614c8b6dd06e59e06225c38ac9232aeeb04b8
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2022-06-06 11:34:23 -0400

    solve bug breakline

--- process_tweets.py
+++ process_tweets.py
@@ -43,16 +43,20 @@ def strip_all_entities(text):
 
 pre_trained_model = spacy.load("en_core_web_sm")
 
-fn = "D:/u/tesis/twitterStream-20091110-20100201-v0.1.1/twitterStream-20091110-20100201-v0.1.1"  # 40 lines of data
+# fn = "D:/u/tesis/twitterStream-20091110-20100201-v0.1.1/twitterStream-20091110-20100201-v0.1.1"  # 40 lines of data
+# fn = "C:/Users/gabri/Desktop/iwv/proccess_tweets.txt"
 
 bat_size = 256 
 buff_size = 2048  # a multiple of bat_size
 emp_ldr = TweetStreamLoader(fn, bat_size, buff_size, shuffle=False) 
 
 for (b_idx, batch) in enumerate(emp_ldr):
-    with open('proccess_tweets.txt', 'w', encoding='utf-8') as wr:
+    with open('tweets.txt', 'w', encoding='utf-8') as wr:
         for tweet in batch:
-            tweet = tweet.split('\t')[2]
+            split = tweet.split('\t')
+            if len(split) != 3:
+                continue
+            tweet = split[2]
             detection = spacy_language_detection(tweet, pre_trained_model)
             if detection['language'] == 'en' and detection['score'] >= 0.85:
                 tweet = strip_all_entities(strip_links(tweet)).rstrip('\n')
